edb40a41321a3c5f7507a3e3e7db97d40c334d98
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-03 03:32:45 +0900

    Prepare a bridge for wanedge

--- kvm-guests/90-vteskins/init-bridges.sh
+++ kvm-guests/90-vteskins/init-bridges.sh
@@ -6,11 +6,14 @@
 set -e
 set -o pipefail
 
-ip_mng_br0=192.168.2.1
-net_mng_br0=192.168.2.0
+ip_mng_br0="192.168.2.1/24"
+
+ip_wanedge="10.255.0.1/24"
+
 name_ovs_br0=vnet-itest-0
 name_ovs_br1=vnet-itest-1
 name_ovs_br2=vnet-itest-2
+name_ovs_wanedge=vnet-wanedge
 name_mng_br0=vnet-br0
 
 sudo ip link set ${name_ovs_br0} down || true
@@ -28,9 +31,14 @@ sudo brctl delbr ${name_ovs_br2} || true
 sudo brctl addbr ${name_ovs_br2}
 sudo ip link set ${name_ovs_br2} up
 
+sudo ip link set ${name_ovs_wanedge} down || true
+sudo brctl delbr ${name_ovs_wanedge} || true
+sudo brctl addbr ${name_ovs_wanedge}
+sudo ip link set ${name_ovs_wanedge} up
+sudo ip addr add ${ip_wanedge} dev ${name_ovs_wanedge}
+
 sudo ip link set ${name_mng_br0} down || true
 sudo brctl delbr ${name_mng_br0} || true
 sudo brctl addbr ${name_mng_br0}
-sudo ifconfig ${name_mng_br0} ${ip_mng_br0} up
-
-sudo route add -net ${net_mng_br0} netmask 255.255.255.0 gw ${ip_mng_br0} ${name_mng_br0}
+sudo ip link set ${name_mng_br0} up
+sudo ip addr add ${ip_mng_br0} dev ${name_mng_br0}
