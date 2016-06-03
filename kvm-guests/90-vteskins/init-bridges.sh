4ab9c83a103b8ec87d0846ac9a0897bfd779cbbc
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-04 00:17:00 +0900

    Add masquerade rule for internet access

--- kvm-guests/90-vteskins/init-bridges.sh
+++ kvm-guests/90-vteskins/init-bridges.sh
@@ -8,6 +8,7 @@ set -o pipefail
 
 ip_mng_br0="192.168.2.1/24"
 
+network_wanedge="10.255.0.0/24"
 ip_wanedge="10.255.0.1/24"
 
 name_ovs_br0=vnet-itest-0
@@ -42,3 +43,5 @@ sudo brctl delbr ${name_mng_br0} || true
 sudo brctl addbr ${name_mng_br0}
 sudo ip link set ${name_mng_br0} up
 sudo ip addr add ${ip_mng_br0} dev ${name_mng_br0}
+
+sudo iptables -t nat -A POSTROUTING -s ${network_wanedge} -j MASQUERADE
