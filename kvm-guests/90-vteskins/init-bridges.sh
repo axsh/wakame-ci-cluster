935f9a3b9f928c18e7c57dc14d0e88c1ab4dfb95
Akira Yokokawa <a-yokokawa@axsh.net>
2016-06-04 18:50:03 +0900

    Add macvlan interface

--- kvm-guests/90-vteskins/init-bridges.sh
+++ kvm-guests/90-vteskins/init-bridges.sh
@@ -7,15 +7,17 @@ set -e
 set -o pipefail
 
 ip_mng_br0="192.168.2.1/24"
-
-network_wanedge="10.255.0.0/24"
-ip_wanedge="10.255.0.1/24"
+ip_wanedge="10.210.0.1/24"
 
 name_ovs_br0=vnet-itest-0
 name_ovs_br1=vnet-itest-1
 name_ovs_br2=vnet-itest-2
 name_ovs_wanedge=vnet-wanedge
 name_mng_br0=vnet-br0
+
+macvlan_1_name="testtap"
+macvlan_1_mac="10:AA:FF:10:AA:01"
+macvlan_1_ip="10.210.0.99/24"
 
 sudo ip link set ${name_ovs_br0} down || true
 sudo brctl delbr ${name_ovs_br0} || true
@@ -43,6 +45,9 @@ sudo brctl delbr ${name_mng_br0} || true
 sudo brctl addbr ${name_mng_br0}
 sudo ip link set ${name_mng_br0} up
 sudo ip addr add ${ip_mng_br0} dev ${name_mng_br0}
+
+sudo ip link add link ${name_ovs_wanedge} dev ${macvlan_1_name} address ${macvlac_1_mac} type macvlan
+sudo ip addr add ${macvlan_1_ip} dev ${macvlan_1_name}
 
 sudo iptables -t nat -D POSTROUTING -s ${network_wanedge} -j MASQUERADE || :
 sudo iptables -t nat -A POSTROUTING -s ${network_wanedge} -j MASQUERADE
