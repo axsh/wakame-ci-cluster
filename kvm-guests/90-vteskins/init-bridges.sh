8795f2090df8475f05cbe3358f4a87013dd7f5d1
Metallion <akieckens@axsh.net>
2016-10-05 17:42:22 +0900

    Fixed a typo and a missing variable in init-bridges.sh

--- kvm-guests/90-vteskins/init-bridges.sh
+++ kvm-guests/90-vteskins/init-bridges.sh
@@ -3,11 +3,12 @@
 # requires:
 #  bash
 #
-set -e
+set -xe
 set -o pipefail
 
 ip_mng_br0="192.168.2.1/24"
 ip_wanedge="10.210.0.1/24"
+network_wanedge="10.210.0.0/24"
 
 name_ovs_br0=vnet-itest-0
 name_ovs_br1=vnet-itest-1
@@ -46,7 +47,7 @@ sudo brctl addbr ${name_mng_br0}
 sudo ip link set ${name_mng_br0} up
 sudo ip addr add ${ip_mng_br0} dev ${name_mng_br0}
 
-sudo ip link add link ${name_ovs_wanedge} dev ${macvlan_1_name} address ${macvlac_1_mac} type macvlan
+sudo ip link add link ${name_ovs_wanedge} dev ${macvlan_1_name} address ${macvlan_1_mac} type macvlan
 sudo ip addr add ${macvlan_1_ip} dev ${macvlan_1_name}
 
 sudo iptables -t nat -D POSTROUTING -s ${network_wanedge} -j MASQUERADE || :
