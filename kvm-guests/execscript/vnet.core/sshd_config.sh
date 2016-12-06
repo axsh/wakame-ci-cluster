38fdcdb588ca25eb5263f1e799dc411fb3d4bcb3
rakshasa <sundell.software@gmail.com>
2016-12-06 18:38:56 +0900

    Disable UseDNS and GSS api for sshd on vnet.core.

--- kvm-guests/execscript/vnet.core/sshd_config.sh
+++ kvm-guests/execscript/vnet.core/sshd_config.sh
@@ -12,5 +12,7 @@ sed -i \
  -e 's,^PermitRootLogin .*,PermitRootLogin yes,' \
  -e 's,^PasswordAuthentication .*,PasswordAuthentication yes,' \
  -e 's,^DenyUsers.root,#DenyUsers root,' \
+ -e 's,^UseDNS .*,UseDNS no,' \
+ -e 's,^GSSAPIAuthentication .*,GSSAPIAuthentication no,' \
  \
  ${chroot_dir}/etc/ssh/sshd_config
